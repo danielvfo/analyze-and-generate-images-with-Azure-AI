0cce0fcd6f485dde5c0478ee40b41a963e403d11
Daniel <danielvfo@gmail.com>
2023-12-02 00:28:31 -0300

    Setup Azure AI Image Analysis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
+import { analyzeImage } from './azure-image-analysis';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [analysisResults, setAnalysisResults] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleAnalyze = async () => {
+    setIsLoading(true);
+    const results = await analyzeImage(imageUrl, 'Categories,Description,Color');
+    setAnalysisResults(results);
+    setIsLoading(false);
+  };
+
+  const DisplayResults = () => (
+    <div>
+      <h2>Image URL: {imageUrl}</h2>
+      <img src={imageUrl} alt='' height='250px'></img>
+      <pre>{JSON.stringify(analysisResults, null, 2)}</pre>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>Computer Vision</h1>
+        <br />
+        <p>Insert URL or type prompt</p>
+        <input type="text" value={imageUrl} onChange={e => setImageUrl(e.target.value)} />
+        <br />
+        <button onClick={handleAnalyze} disabled={isLoading}>
+          {isLoading ? 'Analyzing...' : 'Analyze'}
+        </button>
+        {/* <button type="submit">Generate</button> */}
+      </div>
+      {analysisResults && <DisplayResults />}
     </div>
   );
 }
---
+++ src/azure-image-analysis.js
@@ -0,0 +1,27 @@
+// azure-image-analysis.js
+import axios from 'axios';
+
+const subscriptionKey = process.env.REACT_APP_SUBSCRIPTION_KEY;
+const endpoint = process.env.REACT_APP_ENDPOINT;
+
+export async function analyzeImage(imageUrl, features) {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscriptionKey
+  };
+
+  const params = {
+    'visualFeatures': features,
+    'details': '',
+    'language': 'en'
+  };
+
+  const body = {
+    url: imageUrl
+  };
+
+  console.log();
+
+  const response = await axios.post(`${endpoint}computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read,caption`, body, { headers });
+  return response.data;
+}
